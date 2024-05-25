82b2f71a8b61f8c32aef916551eac23ade8502fb
wzzYtu <493189841@qq.com>
2024-05-25 22:29:33 +0800

    hello cosmos

---
+++ hello/x/hello/genesis_test.go
@@ -0,0 +1,31 @@
+package hello_test
+
+import (
+	"testing"
+
+	keepertest "hello/testutil/keeper"
+	"hello/testutil/nullify"
+	"hello/x/hello"
+	"hello/x/hello/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params:	types.DefaultParams(),
+		
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.HelloKeeper(t)
+	hello.InitGenesis(ctx, *k, genesisState)
+	got := hello.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
